7479bc9d39f6b96218d6ba650c019197df1695e3
C.Seng <74439890+cheeseig9@users.noreply.github.com>
2023-04-02 00:16:18 +0800

    finish

--- jupiter-website-ecs/variables.tf
+++ jupiter-website-ecs/variables.tf
@@ -1,8 +1,5 @@
-#environment variables
 variable "region" {}
 variable "project_name" {}
-
-#VPC variables 
 variable "vpc_cidr" {}
 variable "public_subnet_az1_cidr" {}
 variable "public_subnet_az2_cidr" {}
@@ -10,3 +7,5 @@ variable "private_app_subnet_az1_cidr" {}
 variable "private_app_subnet_az2_cidr" {}
 variable "private_data_subnet_az1_cidr" {}
 variable "private_data_subnet_az2_cidr" {}
+variable "domain_name" {}
+variable "alternative_name" {}
