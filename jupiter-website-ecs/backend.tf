f72ee7c02f08407d71b8a3e07a74cd38e6e689d2
C.Seng <74439890+cheeseig9@users.noreply.github.com>
2023-04-01 23:16:45 +0800

    first-terraform-tutorial
    test

---
+++ jupiter-website-ecs/backend.tf
@@ -0,0 +1,9 @@
+# # store the terraform state file in s3
+# terraform {
+#   backend "s3" {
+#     bucket    = "cheeseng-terraform-remote-state-s3-1"
+#     key       = "jupiter-website-ecs.tfstate"
+#     region    = "us-east-1"
+#     profile   = "terraform-user"
+#   }
+# }
